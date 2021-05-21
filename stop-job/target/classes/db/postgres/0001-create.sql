62e68696247d3d403b7bbe68eebc4479cde3a0a4
Meirelles de Rezende <ared@gft.com>
2021-05-21 09:51:49 -0300

    removed invalid directories

--- stop-job/target/classes/db/postgres/0001-create.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE interop.batch_parameters(id serial primary key, name varchar(80) unique not null, property_value varchar(200) not null);
-INSERT INTO interop.batch_parameters (id, name, property_value) values (1, 'enabled','false');
-
-CREATE TABLE interop.user (id serial primary key, name varchar(80) unique not null default '');
-INSERT INTO interop.user (id, name) select generate_series(1, 20000), 'teste' || generate_series(1, 20000);
